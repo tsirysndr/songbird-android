a5654d59236e3112e30366bebf7ce98cc6120c20
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-04-07 23:18:30 +0300

    update exported function name
    update exported function name
    
    use android_logger

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,15 @@
+#[macro_use] extern crate log;
+
+use android_logger::Config;
+use log::LevelFilter;
+
 #[no_mangle]
-#[export_name = "example"]
-extern "C" fn example() {
-    println!("Hello Android!");
+#[export_name = "Java_com_tsirysndr_songbird_Songbird_00024Companion_example"]
+pub extern "C" fn example() {
+    android_logger::init_once(Config::default().with_max_level(LevelFilter::Trace));
+    debug!("Hello Android!");
+    debug!("this is a debug {}", "message");
+    error!("this is printed by default");
 }
 
 pub fn add(left: usize, right: usize) -> usize {
